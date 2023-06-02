2acab63e79bec15967a737075a9ba6f7f4b23e27
ThatFunnyWeirdo <112593486+ThatFunnyWeirdo@users.noreply.github.com>
2023-06-01 21:26:43 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+sketches = ["alarm_clock", "apple", "pen", "camera", "sweater", "book"];
+
+random_number = Math.floor((Math.random()*6)+1);
+
+timer_counter = 0;
+
+timer_check = "";
+
+drawn_sketch = "";
+
+answer_holder = "";
+
+score = 0;
